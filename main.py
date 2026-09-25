fb3007bef31bb02e455731456501134e2c80ccd8
mplampou-crypto <mplampou@gmail.com>
2026-09-26 02:45:16 +0300

    zc

--- main.py
+++ main.py
@@ -1356,7 +1356,7 @@ async def process_checkout(callback: CallbackQuery, state: FSMContext):
                     pass
             
             # Μήνυμα & Smart Link Χρήστη
-            encoded_text = urllib.parse.quote(f"Γεια σου Δήμητρα! Αγόρασα custom παραγγελία. Ο κωδικός μου είναι #{order_code} και θέλω να συνεννοηθούμε για την κατασκευή.")
+            encoded_text = urllib.parse.quote(f"Γεια σου Δήμητρα! Ο κωδικός μου είναι #{order_code}.")
             smart_link = f"{ADMIN_LINK}?text={encoded_text}"
             
             kb = InlineKeyboardMarkup(inline_keyboard=[[
